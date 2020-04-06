3954b0804bbbd7807fcd0b3034361f483595c951
Colby-PDQ <colby.bouma+github@pdq.com>
2020-04-06 12:11:37 -0600

    Add User Last Logged On

---
+++ PowerShell Scanners/User Last Logged On/User Last Logged On.ps1
@@ -0,0 +1,54 @@
+# This script requires that Audit Logon events are enabled in Group Policy and those events are kept for the amount of history preferred
+
+[CmdletBinding()]
+param (
+    [Switch]$Lowercase
+)
+
+$UserArray = New-Object System.Collections.ArrayList
+
+# Query all logon events with id 4624 
+Get-EventLog -LogName "Security" -InstanceId 4624 | ForEach-Object {
+
+    $EventMessage = $_
+    $AccountName = $EventMessage.ReplacementStrings[5]
+    $LogonType = $EventMessage.ReplacementStrings[8]
+
+    if ( $Lowercase ) {
+
+        # Make all usernames lowercase so they group properly in Inventory
+        $AccountName = $AccountName.ToLower()
+
+    }
+
+    # Look for events that contain local or remote logon events, while ignoring Windows service accounts
+    if ( ( $LogonType -in "2", "10" ) -and ( $AccountName -notmatch "^(DWM|UMFD)-\d" ) ) {
+    
+        # Skip duplicate names
+        if ( $UserArray -notcontains $AccountName ) {
+
+            $null = $UserArray.Add($AccountName)
+            
+            # Translate the Logon Type
+            if ( $LogonType -eq "2" ) {
+
+                $LogonTypeName = "Local"
+
+            } elseif ( $LogonType -eq "10" ) {
+
+                $LogonTypeName = "Remote"
+
+            }
+
+            # Build an object containing the Username, Logon Type, and Last Logon time
+            [PSCustomObject]@{
+                Username  = $AccountName
+                LogonType = $LogonTypeName
+                LastLogon = $EventMessage.TimeGenerated.ToString("yyyy-MM-dd HH:mm:ss")
+            }  
+
+        }
+
+    }
+
+}
