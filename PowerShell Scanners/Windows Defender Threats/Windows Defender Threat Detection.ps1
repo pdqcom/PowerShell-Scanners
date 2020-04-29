db52899599d35c61cb9d250a7902d3429690796a
b0park <36933239+b0park@users.noreply.github.com>
2020-04-29 16:57:35 -0400

    Add files via upload
    Initial config.

---
+++ PowerShell Scanners/Windows Defender Threats/Windows Defender Threat Detection.ps1
@@ -0,0 +1,11 @@
+﻿if (-Not(Get-Command Get-MpComputerStatus -ErrorAction SilentlyContinue)) {
+    throw "Unable to find Get-MpComputerStatus. Available on Windows 10/Server 2016 or higher"
+}
+
+$DefenderStatus = (Get-Service WinDefend -ErrorAction SilentlyContinue).Status
+
+if ($DefenderStatus -ne "Running") {
+    throw "The Windows Defender service is not currently running"
+}
+
+Get-MpThreatDetection
