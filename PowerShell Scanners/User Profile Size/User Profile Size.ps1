c52296db922217d9c12c674e974c9349f7830d17
Stephen Valdinger <stephen@chocolatey.io>
2020-04-30 21:37:20 -0400

    Return a friendly size calculation in addition to bytes

--- PowerShell Scanners/User Profile Size/User Profile Size.ps1
+++ PowerShell Scanners/User Profile Size/User Profile Size.ps1
@@ -1,15 +1,29 @@
+[cmdletBinding()]
+param(
+    [ValidateSet('KB','MB','GB')]
+    [string]
+    $In = 'MB'
+)
+
 $ErrorActionPreference = "SilentlyContinue"
 
 $UserFolders = Get-ChildItem -Path "C:\Users" -Force -Directory
 
 ForEach ( $Folder in $UserFolders ) {
 
-    [UInt64]$FolderSize = ( Get-Childitem -Path $Folder.FullName -Force -Recurse | Measure-Object -Property "Length" -Sum ).Sum
+    $FolderSize = ( Get-Childitem -Path $Folder.FullName -Force -Recurse | Measure-Object -Property "Length" -Sum ).Sum
+    
+    switch($In){
+        'KB' {$FriendlySize = "$([math]::Round(($FolderSize /1KB),2)) KB"}
+        'MB' {$FriendlySize = "$([math]::Round(($FolderSize /1MB),2)) MB"}
+        'GB' {$FriendlySize = "$([math]::Round(($FolderSize /1GB),2)) GB"}
+    }
     
     [PSCustomObject]@{
         FolderName    = $Folder.BaseName
         FolderPath    = $Folder.FullName
         Size          = $FolderSize
+        FriendlySize  = $FriendlySize
     }
 
 }
