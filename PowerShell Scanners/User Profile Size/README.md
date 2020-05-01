88793024c33be76d57a8602b3bc86d58eb1285d0
Stephen Valdinger <stephen@chocolatey.io>
2020-04-30 23:53:41 -0400

    Revert "Return a friendly size calculation in addition to bytes"
    This reverts commit c52296db922217d9c12c674e974c9349f7830d17.

--- PowerShell Scanners/User Profile Size/README.md
+++ PowerShell Scanners/User Profile Size/README.md
@@ -4,25 +4,5 @@ Recursively scans every folder in `C:\Users`, then adds up the size of every fil
 
 This scanner may take a while to run!
 
-## Parameters
-
-```powershell
--In
-```
-
-Accepts 'KB','MB', or 'GB' and returns friendly size representative of the chosen increment, rounded to 2 decimal places.
-
-Defaults to 'MB'.
-
-## Sample Output
-
-```powershell
-FolderName    FolderPath                   Size FriendlySize
-----------    ----------                   ---- ------------
-Administrator C:\Users\Administrator  496406547 473.41 MB
-All Users     C:\Users\All Users     7630976535 7277.47 MB
-```
-
-## Author
-
+# Author
 Nate Blevins
--- PowerShell Scanners/User Profile Size/User Profile Size.ps1
+++ PowerShell Scanners/User Profile Size/User Profile Size.ps1
@@ -1,29 +1,15 @@
-[cmdletBinding()]
-param(
-    [ValidateSet('KB','MB','GB')]
-    [string]
-    $In = 'MB'
-)
-
 $ErrorActionPreference = "SilentlyContinue"
 
 $UserFolders = Get-ChildItem -Path "C:\Users" -Force -Directory
 
 ForEach ( $Folder in $UserFolders ) {
 
-    $FolderSize = ( Get-Childitem -Path $Folder.FullName -Force -Recurse | Measure-Object -Property "Length" -Sum ).Sum
-    
-    switch($In){
-        'KB' {$FriendlySize = "$([math]::Round(($FolderSize /1KB),2)) KB"}
-        'MB' {$FriendlySize = "$([math]::Round(($FolderSize /1MB),2)) MB"}
-        'GB' {$FriendlySize = "$([math]::Round(($FolderSize /1GB),2)) GB"}
-    }
+    [UInt64]$FolderSize = ( Get-Childitem -Path $Folder.FullName -Force -Recurse | Measure-Object -Property "Length" -Sum ).Sum
     
     [PSCustomObject]@{
         FolderName    = $Folder.BaseName
         FolderPath    = $Folder.FullName
         Size          = $FolderSize
-        FriendlySize  = $FriendlySize
     }
 
 }
