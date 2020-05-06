8605a9a8a28c69bd25c2cd6863b445295bd2573e
Stephen Valdinger <stephen@chocolatey.io>
2020-05-06 15:23:23 -0400

    Changes from PR review

--- PowerShell Scanners/Windows Firewall Rules/Windows Firewall Rules.ps1
+++ PowerShell Scanners/Windows Firewall Rules/Windows Firewall Rules.ps1
@@ -7,13 +7,13 @@ param(
 
 process{
     
-    if([version](Get-WmiObject win32_operatingsystem).Version -lt 6.3.0){
-        throw "This scanner only available on Server 2012/Windows8 or higher"
+    if([version](Get-CimInstance win32_operatingsystem).Version -lt 6.3.0){
+        throw "This scanner is only available on Server 2012/Windows 8 or higher"
     }
     
     switch($Enabled){
         $true { Get-NetFirewallRule -Enabled True| Select-Object Name,DisplayName,Description,Profile,Direction,Action }
-        $false{ Get-NetFirewallRule | Select-Object Name,DisplayName,Description,Enabled,Profile,Direction,Action }
+        $false { Get-NetFirewallRule | Select-Object Name,DisplayName,Description,Enabled,Profile,Direction,Action }
     }
 
 }
