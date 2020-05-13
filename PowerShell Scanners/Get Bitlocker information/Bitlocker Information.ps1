8edbedfffe25599727d8082618e5e0a5b9b92337
Andrew Baker <10560135+theterminator3000@users.noreply.github.com>
2020-05-13 08:42:13 +0100

    Update Bitlocker Information.ps1

--- PowerShell Scanners/Get Bitlocker information/Bitlocker Information.ps1
+++ PowerShell Scanners/Get Bitlocker information/Bitlocker Information.ps1
@@ -1,5 +1,3 @@
-if (-Not(Get-Command Get-BitlockerVolume -ErrorAction SilentlyContinue)) {
-    throw "Unable to find Get-BitlockerVolume. Available on Windows 10 or Server 2016 with the Bitlocker feature installed"
-}
+#Requires -Modules BitLocker
 
 Get-BitlockerVolume
