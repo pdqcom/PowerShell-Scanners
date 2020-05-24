114a632e1ba07495a96f3227dd5d2e632f1ac961
Colby-PDQ <colby.bouma+github@pdq.com>
2020-05-24 14:24:21 -0600

    Minor formatting changes
    Format Document FTW!

--- PowerShell Scanners/Battery Status/Battery Status.ps1
+++ PowerShell Scanners/Battery Status/Battery Status.ps1
@@ -1,9 +1,9 @@
-﻿function Return-Battery($BatteryName = "No Battery Detected", $DesignedCapacity = 0, $FullCharge = 0, $Health = 0){
+﻿function Return-Battery($BatteryName = "No Battery Detected", $DesignedCapacity = 0, $FullCharge = 0, $Health = 0) {
     [PSCustomObject]@{
-        Name = $BatteryName
+        Name             = $BatteryName
         DesignedCapacity = $DesignedCapacity
-        FullCharge = $FullCharge
-        Health = $Health
+        FullCharge       = $FullCharge
+        Health           = $Health
     }
 }
 
@@ -12,11 +12,11 @@ $Batteries = (Get-WmiObject -Class "BatteryStatus" -Namespace "ROOT\WMI" -ErrorA
 $AllBatteryData = Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI"
 $AllCharges = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity
 $BatteryIndex = 0
-foreach($Battery in $Batteries){
+foreach ($Battery in $Batteries) {
     $BatteryName = $AllBatteryData.DeviceName.split(" ")[$BatteryIndex]
     $DesignedCapacity = $AllBatteryData.DesignedCapacity[$BatteryIndex]
     $FullCharge = $AllCharges[$BatteryIndex]
-    $Health = [math]::Round($FullCharge/$DesignedCapacity * 100,2)
+    $Health = [math]::Round($FullCharge / $DesignedCapacity * 100, 2)
 
     Return-Battery $BatteryName $DesignedCapacity $FullCharge $Health
     $BatteryIndex++
