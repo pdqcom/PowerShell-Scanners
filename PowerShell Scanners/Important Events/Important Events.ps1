df6d7ee3865e90529767f56f03e2cbd04fe1ea63
Colby-PDQ <colby.bouma+github@pdq.com>
2020-05-24 13:51:41 -0600

    A few minor changes
    Capitalization, formatting, etc.

--- PowerShell Scanners/Important Events/Important Events.ps1
+++ PowerShell Scanners/Important Events/Important Events.ps1
@@ -1,26 +1,25 @@
-
 [CmdletBinding()]
 param (
     # Number of days worth of entries to collect
-    [uint]$Days = 28, # Defaults to Four weeks
+    [UInt32]$Days = 28, # Defaults to Four weeks
     # Limit the number of returned events
-    [uint]$EventLimit = 30, # Defaults to 30 events
-    [ValidateSet("System","Application")]
-    [string]$EventLog = "System", # Defaults to "System"
+    [UInt32]$EventLimit = 30, # Defaults to 30 events
+    [ValidateSet("System", "Application")]
+    [String]$EventLog = "System", # Defaults to "System"
     # The level of events to gather
-    [ValidateRange(0,4)]
-    [uint]$EventLevel = 1 # Critical
+    [ValidateRange(0, 4)]
+    [UInt32]$EventLevel = 1 # Critical
 )
 
 # Set the start date to be $Days before now
-$StartDate = (get-date).adddays(-$Days)
+$StartDate = (Get-Date).AddDays(-$Days)
 
 # Collect and output all relevant events
-Get-WinEvent -FilterHashtable @{logname=$EventLog; Level=$EventLevel; starttime=$StartDate} -ErrorAction SilentlyContinue | Select-Object -Last $EventLimit | ForEach-Object {
+Get-WinEvent -FilterHashtable @{LogName = $EventLog; Level = $EventLevel; StartTime = $StartDate } -ErrorAction SilentlyContinue | Select-Object -Last $EventLimit | ForEach-Object {
     [PSCustomObject]@{
-        Id = $_.Id
-        Provider = $_.ProviderName
-        Message = $_.Message
-        TimeCreated = [datetime]$_.TimeCreated.ToString("yyyy-MM-dd HH:mm:ss")
+        Id          = $_.Id
+        Provider    = $_.ProviderName
+        Message     = $_.Message
+        TimeCreated = [DateTime]$_.TimeCreated.ToString("yyyy-MM-dd HH:mm:ss")
     }
 }
