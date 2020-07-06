00503b5e56d3f15c876cde920d4d57a47b83ae2b
Tyson Fewins <13089078+tfewins@users.noreply.github.com>
2020-07-06 16:17:14 -0400

    Update Hosts File.ps1
    Removed the unnecessary arraylist that gathered returned objects. Some edits to formatting and comments.

--- PowerShell Scanners/Hosts File/Hosts File.ps1
+++ PowerShell Scanners/Hosts File/Hosts File.ps1
@@ -1,9 +1,7 @@
 #Get the contents of the hosts file
 $HostFile = Get-Content -Path "C:\Windows\System32\drivers\etc\hosts"
 
-#Add valid host entries into an arraylist
-#Collect the IP address, hostname, and comments
-[System.Collections.ArrayList]$HostsFileEntries = @()
+#Iterate through each line to return the IP Address, Hostname, and Comments for each entry
 foreach($line in $HostFile){
 
     #skip commented lines and empty lines in the file
@@ -12,19 +10,21 @@ foreach($line in $HostFile){
     #Split the entry by whitespace to target the fields independently
     $entrytext = $line.ToString().Trim()
     [System.Collections.ArrayList]$entryArray = $entrytext -split "\s+"
+    
+    #Assign IPAddress and HostName
     $IPAddress = $entryArray[0]
     $HostName = $entryArray[1]
+    
     #Remove the $IPAddress and $HostName from the $entryArray
     $entryArray.RemoveRange(0,2)
-    #Convert $entryArray back to a string for the comments entry
+    
+    #Convert $entryArray back to a string for the Comments field
     $Comments = [string]$entryArray
 
-    #Add host file entry to custom object
-    $obj = [PSCustomObject]@{
+    #Return host file entry
+    [PSCustomObject]@{
         IPAddress = $IPAddress
         HostName = $HostName
         Comments = $Comments
     }
-    $HostsFileEntries.Add($obj) | Out-Null
 }
-return $HostsFileEntries
