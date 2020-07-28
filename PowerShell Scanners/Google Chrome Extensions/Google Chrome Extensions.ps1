a4fd540a98d33a4b4d8055aadbb3a9084884c7a8
Colby-PDQ <colby.bouma+github@pdq.com>
2020-07-28 10:05:59 -0600

    Use a .NET method to parse JSON
    Potential fix for #23
    
    Unfortunately, this method is case sensitive, so I had to remove the capitilzation from all the property names :'(

--- PowerShell Scanners/Google Chrome Extensions/Google Chrome Extensions.ps1
+++ PowerShell Scanners/Google Chrome Extensions/Google Chrome Extensions.ps1
@@ -34,14 +34,18 @@ Foreach ( $User in (Get-ChildItem -Directory -Path "$env:SystemDrive\Users") ) {
     }
 
     # Read Preferences JSON file
-    $PreferencesJson = Get-Content $PreferencesFile | ConvertFrom-Json
+    # This .NET method is necessary because ConvertFrom-Json can't handle duplicate entries with different cases.
+    # https://github.com/pdq/PowerShell-Scanners/issues/23
+    Add-Type -AssemblyName System.Web.Extensions
+    $PreferencesText = Get-Content $PreferencesFile
+    $JsonParser = [System.Web.Script.Serialization.JavaScriptSerializer]::New()
+    $PreferencesJson = $JsonParser.DeserializeObject($PreferencesText)
 
-    # https://andrewpla.dev/Iterate-Over-Object-Properties/
-    Foreach ( $Extension in (($PreferencesJson.Extensions.Settings).PSObject.Members | Where-Object MemberType -eq 'NoteProperty') ) {
+    Foreach ( $Extension in $PreferencesJson.extensions.settings.GetEnumerator() ) {
 
-        $ID = $Extension.Name
+        $ID = $Extension.Key
         $Extension = $Extension.Value
-        $Name = $Extension.Manifest.Name
+        $Name = $Extension.manifest.name
 
         # Ignore blank names
         if ( -Not $Name ) {
@@ -52,7 +56,7 @@ Foreach ( $User in (Get-ChildItem -Directory -Path "$env:SystemDrive\Users") ) {
         }
             
         # Convert Install_Time
-        $InstallTime = [Double]$Extension.Install_Time
+        $InstallTime = [Double]$Extension.install_time
         # Divide by 1,000,000 because we are going to add seconds on to the base date
         $InstallTime = ($InstallTime - 11644473600000000) / 1000000
         $UtcTime = Get-Date -Date "1970-01-01 00:00:00"
@@ -61,22 +65,22 @@ Foreach ( $User in (Get-ChildItem -Directory -Path "$env:SystemDrive\Users") ) {
     
         $Output = [Ordered]@{
             Name           = [String]  $Name
-            Version        = [String]  $Extension.Manifest.Version
-            Enabled        = [Bool]    $Extension.State
-            Description    = [String]  $Extension.Manifest.Description
-            DefaultInstall = [Bool]    $Extension.Was_Installed_By_Default
-            OemInstall     = [Bool]    $Extension.Was_Installed_By_OEM
+            Version        = [String]  $Extension.manifest.version
+            Enabled        = [Bool]    $Extension.state
+            Description    = [String]  $Extension.manifest.description
+            DefaultInstall = [Bool]    $Extension.was_installed_by_default
+            OemInstall     = [Bool]    $Extension.was_installed_by_oem
             ID             = [String]  $ID
             InstallDate    = [DateTime]$LocalTime
             User           = [String]  $User
-            ChromeVer      = [String]  $PreferencesJson.Extensions.Last_Chrome_Version
+            ChromeVer      = [String]  $PreferencesJson.extensions.last_chrome_version
         }
 
         if ( $EnablePermissions ) {
 
             # Convert Permissions array into a multi-line string
             # This multi-line string is kind of ugly in Inventory, so it's disabled by default
-            $Output.Permissions = [String]($Extension.Manifest.Permissions -Join "`n")
+            $Output.Permissions = [String]($Extension.manifest.permissions -Join "`n")
 
         }
 
