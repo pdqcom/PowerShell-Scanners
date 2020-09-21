4829eec697bf41c7a36d9f01eee0656526c3fe5c
Sladersca <Jeremy Gruttner@lan.ffpltc.ca>
2020-09-21 14:59:39 -0500

    Switched commandlet calls away from WindowsUpdateProvider to matching methods in PSWindowsUpdate

--- PowerShell Scanners/Windows Update Last Installed/Windows Update Last Installed.ps1
+++ PowerShell Scanners/Windows Update Last Installed/Windows Update Last Installed.ps1
@@ -1,7 +1,11 @@
 & '.\Install and Import Module.ps1' -ModuleName "PSWindowsUpdate"
 
+# Store results in a temp variable
+$lastResults = Get-WULastResults
+
+#organize results in a PSCustomObject
 [PSCustomObject]@{
-    LastInstallationDate = [DateTime](Get-WULastInstallationDate)
-    LastScanSuccessDate  = [DateTime](Get-WULastScanSuccessDate)
-    IsPendingReboot      = [Bool](Get-WUIsPendingReboot)
+    LastInstallationDate = [DateTime] $lastResults.LastInstallationSuccessDate
+    LastScanSuccessDate  = [DateTime] $lastResults.LastSearchSuccessDate
+    IsPendingReboot      = [Bool] (Get-WURebootStatus)
 }
