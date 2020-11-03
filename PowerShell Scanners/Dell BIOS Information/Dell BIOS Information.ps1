f1e0a76c3334706f95cd627d753dfb48f060c7ae
Colby-PDQ <colby.bouma+github@pdq.com>
2020-11-03 12:49:31 -0700

    Fix formatting

--- PowerShell Scanners/Dell BIOS Information/Dell BIOS Information.ps1
+++ PowerShell Scanners/Dell BIOS Information/Dell BIOS Information.ps1
@@ -1,15 +1,15 @@
-IF (Get-WmiObject -Class:Win32_ComputerSystem -Filter:"Manufacturer LIKE '%Dell%'"){
-$null = Install-Module DellBIOSProvider -force
-$null = Import-Module DellBIOSProvider -force
-Get-ChildItem dellsmbios:\ | ForEach-Object {
-$Category = $_.Category
-Get-ChildItem dellsmbios:\"$Category" | ForEach-Object {
-[PSCustomObject]@{
-Category = $Category
-Attribute = $_.Attribute
-Description = $_.Description
-CurrentValue = $_.CurrentValue
-}
+if (Get-CimInstance -ClassName Win32_ComputerSystem -Filter:"Manufacturer LIKE '%Dell%'") {
+    $null = Install-Module DellBIOSProvider -Force
+    $null = Import-Module DellBIOSProvider -Force
+    Get-ChildItem DellSmbios:\ | ForEach-Object {
+        $Category = $_.Category
+        Get-ChildItem DellSmbios:\"$Category" | ForEach-Object {
+            [PSCustomObject]@{
+                Category     = $Category
+                Attribute    = $_.Attribute
+                Description  = $_.Description
+                CurrentValue = $_.CurrentValue
+            }
+        } 
+    }
 } 
-}
-}
