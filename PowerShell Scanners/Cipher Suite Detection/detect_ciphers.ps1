0b6b0f95ff60365e940e91fd5d182be703488996
Timothy Bernard <darby.bernard@gmail.com>
2021-01-13 14:50:46 -0500

    Fixed logic for non-existent keys

--- PowerShell Scanners/Cipher Suite Detection/detect_ciphers.ps1
+++ PowerShell Scanners/Cipher Suite Detection/detect_ciphers.ps1
@@ -1,75 +1,152 @@
 #Query Registry for cipher suites
 
 #SSLv2
-$SSLv2Server = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -Name 'Enabled'
-$SSLv2Client = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Name 'Enabled'
+$SSLv2Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server'
+$SSLv2Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' 
 
 #SSLv3
-$SSLv3Server = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -Name 'Enabled'
-$SSLv3Client = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -Name 'Enabled'
+$SSLv3Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' 
+$SSLv3Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' 
 
 #TLSv1.0
-$TLSv1Server = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'Enabled'
-$TLSv1Client = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'Enabled'
+$TLSv1Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' 
+$TLSv1Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' 
 
 #TLSv1.1
-$TLSv1_1Server = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'Enabled'
-$TLSv1_1Client = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'Enabled'
+$TLSv1_1Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' 
+$TLSv1_1Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' 
 
 
 #Check if enabled
 
-if( $SSLv2Server.Enabled -eq 0){
-  $SSLv2ServerEnabled = $false
+if (Test-Path -Path $SSLv2Server) {
+
+    if ((Get-ItemPropertyValue -Path $SSLv2Server -Name Enabled -ErrorAction SilentlyContinue) -eq "0") {
+        $SSLv2ServerEnabled = $false
+    }
+
+    if ((Get-ItemPropertyValue -Path $SSLv2Server -Name Enabled -ErrorAction SilentlyContinue) -eq "1") {
+        $SSLv2ServerEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $SSLv2Server)){
+    $SSLv2ServerEnabled = "Key not found in registry."
+} else {
+    $SSLv2ServerEnabled = "Unknown issue occured. Requires manual review."
+}
+
+
+if(Test-Path -Path $SSLv2Client){
+
+    if((Get-ItemPropertyValue -Path $SSLv2Client -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $SSLv2ClientEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $SSLv2Client -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
+        $SSLv2ClientEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $SSLv2Client)){
+    $SSLv2ClientEnabled = "Key not found in registry"
 } else{
-    $SSLv2ServerEnabled = $true
+      $SSLv2ClientEnabled = "Unknown issue occured. Requires manual review."
 }
 
-if( $SSLv2Client.Enabled -eq 0){
-    $SSLv2ClientEnabled = $false
-  } else{
-      $SSLv2ClientEnabled = $true
+if(Test-Path -Path $SSLv3Server){
+
+    if((Get-ItemPropertyValue -Path $SSLv3Server -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $SSLv3ServerEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $SSLv3Server -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
+        $SSLv3ServerEnabled = $true
+    }
+
+} elseif(-Not (Test-Path $SSLv3Server)){
+    $SSLv3ServerEnabled = "Key not found in registry"
+} else{
+      $SSLv3ServerEnabled = "Unknown issue occured. Requires manual review."
 }
 
+if(Test-Path -Path $SSLv3Client){
 
-if( $SSLv3Server.Enabled -eq 0){
-    $SSLv3ServerEnabled = $false
-  } else{
-      $SSLv3ServerEnabled = $true
-}
-  
-if( $SSLv3Client.Enabled -eq 0){
-    $SSLv3ClientEnabled = $false
-    } else{
+    if( (Get-ItemPropertyValue -Path $SSLv3Client -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $SSLv3ClientEnabled = $false
+    }
+
+    if( (Get-ItemPropertyValue -Path $SSLv3Client -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
         $SSLv3ClientEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $SSLv3Client)){
+    $SSLv3ClientEnabled = "Key not found in registry"
+} else{
+        $SSLv3ClientEnabled = "Unknown issue occured. Requires manual review."
 }
 
+if(Test-Path -Path $TLSv1Server){
 
-if( $TLSv1Server.Enabled -eq 0){
-    $TLSv1ServerEnabled = $false
+    if((Get-ItemPropertyValue -Path $TLSv1Server -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $TLSv1ServerEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $TLSv1Server -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
+        $TLSv1ServerEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $TLSv1Server)){
+    $TLSv1ServerEnabled = "Key not found in registry"
 } else{
-      $TLSv1ServerEnabled = $true
+      $TLSv1ServerEnabled = "Unknown issue occured. Requires manual review."
 }
  
+if(Test-Path -Path $TLSv1Client){
 
-if( $TLSv1Client.Enabled -eq 0){
-      $TLSv1ClientEnabled = $false
-} else{
+    if((Get-ItemPropertyValue -Path $TLSv1Client -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $TLSv1ClientEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $TLSv1Client -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
         $TLSv1ClientEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $TLSv1Client)){
+    $TLSv1ClientEnabled = "Key not found in registry"
+} else{
+        $TLSv1ClientEnabled = "Unknown issue occured. Requires manual review."
 }
 
+if(Test-Path -Path $TLSv1_1Server){
 
-if( $TLSv1_1Server.Enabled -eq 0){
-    $TLSv1_1ServerEnabled = $false
+    if((Get-ItemPropertyValue -Path $TLSv1_1ServerEnabled -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $TLSv1_1ServerEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $TLSv1_1ServerEnabled -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
+        $TLSv1_1ServerEnabled = $true
+    }
+
+} elseif(-Not (Test-Path -Path $TLSv1_1Server)){
+    $TLSv1_1ServerEnabled = "Key not found in registry"
 } else{
-      $TLSv1_1ServerEnabled = $true
+      $TLSv1_1ServerEnabled = "Unknown issue occured. Requires manual review."
 }
  
+if(Test-Path -Path $TLSv1_1Client){
 
-if( $TLSv1_1Client.Enabled -eq 0){
-      $TLSv1_1ClientEnabled = $false
-} else{
+    if((Get-ItemPropertyValue -Path $TLSv1_1Client -Name Enabled -ErrorAction SilentlyContinue) -eq "0"){
+        $TLSv1_1ClientEnabled = $false
+    }
+
+    if((Get-ItemPropertyValue -Path $TLSv1_1Client -Name Enabled -ErrorAction SilentlyContinue) -eq "1"){
         $TLSv1_1ClientEnabled = $true
+    }
+
+
+} elseif(-Not (Test-Path -Path $TLSv1_1Client)){
+    $TLSv1_1ClientEnabled ="Key not found in registry"
+} else{
+        $TLSv1_1ClientEnabled = "Unknown issue occured. Requires manual review."
 }
 
 
