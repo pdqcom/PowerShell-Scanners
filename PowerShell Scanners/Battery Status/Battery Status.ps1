d01cd1dcac6e81127149c8c0cc7925aa8ab5f53e
ColbyBouma <78135310+ColbyBouma@users.noreply.github.com>
2021-03-11 15:20:15 -0700

    Refactor 'Battery Status'
    Fixes #49
    
    While fixing #49, I decided to refactor this script a bit. Replace Get-WmiObject with Get-CimInstance, remove the unnecessary custom function, etc.

--- PowerShell Scanners/Battery Status/Battery Status.ps1
+++ PowerShell Scanners/Battery Status/Battery Status.ps1
@@ -1,23 +1,33 @@
-﻿function Format-Battery($BatteryName = "No Battery Detected", $DesignedCapacity = 0, $FullCharge = 0, $Health = 0) {
+﻿$Batteries = (Get-WmiObject -Class "BatteryStatus" -Namespace "ROOT\WMI" -ErrorAction SilentlyContinue)
+$AllBatteryData = Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI"
+$AllCharges = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity
+$BatteryIndex = 0
+
+Foreach ($Battery in $Batteries) {
+
+    $BatteryName = $AllBatteryData.DeviceName.split(" ")[$BatteryIndex]
+    $DesignedCapacity = $AllBatteryData.DesignedCapacity[$BatteryIndex]
+    $FullCharge = $AllCharges[$BatteryIndex]
+
+    # https://github.com/pdq/PowerShell-Scanners/issues/49
+    Try {
+
+        $Health = [Math]::Round($FullCharge / $DesignedCapacity * 100, 2)
+
+    } Catch {
+
+        Write-Warning "DesignedCapacity is 0 or null"
+        $Health = $null
+
+    }
+
     [PSCustomObject]@{
         Name             = $BatteryName
         DesignedCapacity = $DesignedCapacity
         FullCharge       = $FullCharge
         Health           = $Health
     }
-}
 
+    $BatteryIndex ++
 
-$Batteries = (Get-WmiObject -Class "BatteryStatus" -Namespace "ROOT\WMI" -ErrorAction SilentlyContinue)
-$AllBatteryData = Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI"
-$AllCharges = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity
-$BatteryIndex = 0
-foreach ($Battery in $Batteries) {
-    $BatteryName = $AllBatteryData.DeviceName.split(" ")[$BatteryIndex]
-    $DesignedCapacity = $AllBatteryData.DesignedCapacity[$BatteryIndex]
-    $FullCharge = $AllCharges[$BatteryIndex]
-    $Health = [math]::Round($FullCharge / $DesignedCapacity * 100, 2)
-
-    Format-Battery $BatteryName $DesignedCapacity $FullCharge $Health
-    $BatteryIndex++
 }
