641e77de5ee200963d4d9e35297666daa7c87cba
ColbyBouma <78135310+ColbyBouma@users.noreply.github.com>
2021-04-19 22:25:53 -0600

    Add 'New-Scanner'

---
+++ PowerShell Scanners/New-Scanner.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = 'True')]
+    [String]$Name
+)
+
+$null = Copy-Item -Path '.\_Template' -Destination ".\$Name" -Recurse
+
+Set-Location ".\$Name"
+
+Rename-Item -Path ".\Template.ps1" -NewName ".\$Name.ps1"
+
+[XML]$ScanProfile = Get-Content '.\Scan Profile.xml'
+
+$ScanProfile.'AdminArsenal.Export'.ScanProfile.Name = "PS - $Name"
+$ScanProfile.'AdminArsenal.Export'.ScanProfile.Scanners.Scanner.ModifiedDate = [String](Get-Date -Format 'o')
+$ScanProfile.'AdminArsenal.Export'.ScanProfile.Scanners.Scanner.Name = $Name
+$ScanProfile.'AdminArsenal.Export'.ScanProfile.Scanners.Scanner.UID = (New-Guid) -replace '-'
+$ScanProfile.'AdminArsenal.Export'.ScanProfile.Scanners.Scanner.FileName = "C:\PowerShell-Scanners\PowerShell Scanners\$Name\$Name.ps1"
+
+$ScanProfile.Save("$PWD\Scan Profile.xml")
+
+Write-Warning "Don't forget to update the Description in 'Scan Profile.xml'!"
---
+++ PowerShell Scanners/_Template/README.md
@@ -0,0 +1,15 @@
+# Instructions
+[How to use this repository](../../README.md)
+
+# Description
+Describe what your script does, and share any important information about it.
+
+# Requirements
+Optional section where you can list conditions that have to be met before running your script.
+
+# Parameters
+## Parameter name
+Describe what the parameter does.
+
+# Author
+Your name (or username) here.
