50bdcd2a340e5b44ab8abe404d3af9af866f8f83
ColbyBouma <78135310+ColbyBouma@users.noreply.github.com>
2021-05-04 06:55:13 -0600

    Make sure current path matches Setup.ps1

--- PowerShell Scanners/ProduKey/Setup.ps1
+++ PowerShell Scanners/ProduKey/Setup.ps1
@@ -1,6 +1,12 @@
 # Disable progress bars.
 $ProgressPreference = 'SilentlyContinue'
 
+# Save the current path.
+$OriginalPath = $PWD
+
+# Make sure the current path is the same as this script.
+Set-Location (Split-Path $MyInvocation.MyCommand.Source)
+
 Remove-Item -Path '.\ProduKey.exe' -ErrorAction 'SilentlyContinue'
 
 [System.Net.WebClient]::new().DownloadFile('https://www.nirsoft.net/utils/produkey.zip', "$PWD\produkey.zip")
@@ -8,3 +14,6 @@ Remove-Item -Path '.\ProduKey.exe' -ErrorAction 'SilentlyContinue'
 Expand-Archive -Path '.\produkey.zip' -DestinationPath .
 
 Remove-Item -Path '.\produkey.zip', '.\ProduKey.chm', '.\readme.txt'
+
+# Restore the original path.
+Set-Location $OriginalPath
