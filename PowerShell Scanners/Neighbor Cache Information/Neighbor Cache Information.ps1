b9f7f60e83468fcdb9d4e21edf872259382188e5
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2021-06-02 12:04:59 -0400

    added runspace support to script

--- PowerShell Scanners/Neighbor Cache Information/Neighbor Cache Information.ps1
+++ PowerShell Scanners/Neighbor Cache Information/Neighbor Cache Information.ps1
@@ -1,5 +1,8 @@
 [CmdletBinding()]
 param(
+	# Define the maximum number of runspaces. Default value is the number of processor son the machine + 1
+	[int]$MaxRunspaces = ( [int]$env:NUMBER_OF_PROCESSORS + 1),
+
 	# skip NBTStat -A on each entry
 	[Switch]$SkipNbtName,
 
@@ -14,9 +17,21 @@ $neighbors = Get-NetNeighbor | Where-Object {
 	($_.LinkLayerAddress -notlike "01-00-5E-*") -and # IPv4 multicast
 	($_.LinkLayerAddress -notlike "33-33-*") } # IPV6 multicast
 
-# Process each neighbor and add additional information
-foreach ($neighbor in $neighbors) {
+## create runspace pool with min =1 and max = $MaxRunspaces
+$rp = [runspacefactory]::CreateRunspacePool(1,$MaxRunspaces)
+$rp.Open()
 
+$cmds = New-Object -TypeName System.Collections.ArrayList
+
+$neighbors | ForEach-Object {
+ ## create powershell and link to runspace pool
+$psa = [powershell]::Create()
+$psa.RunspacePool = $rp
+
+# add the script
+  [void]$psa.AddScript({
+     param ($neighbor, [switch]$SkipNbtName, [switch]$SkipReverseLookup)
+     
 	if (-not $SkipReverseLookup) {
 
 		# Check DNS for the hostname associated with this IP
@@ -56,4 +71,27 @@ foreach ($neighbor in $neighbors) {
 	}
 	$output
 
+
+   })
+
+   # Add the Neighbor Parameter using the current item in the pipeline
+   [void]$psa.AddParameter('Neighbor',$psitem)
+   if($SkipNbtName) { [void]$psa.AddParameter('SkipNbtName',$true) }
+   if($SkipReverseLookup) {[void]$psa.AddParameter('SkipReverseLookup',$true) }
+   
+   $handle = $psa.BeginInvoke()
+
+   $temp = '' | Select-Object PowerShell, Handle
+   $temp.PowerShell = $psa
+   $temp.Handle = $handle
+
+   [void]$cmds.Add($temp)
 }
+
+## retreive data
+$cmds | ForEach-Object {$_.PowerShell.EndInvoke($_.Handle)}
+
+## clean up
+$cmds | ForEach-Object {$_.PowerShell.Dispose()}  
+$rp.Close()
+$rp.Dispose() 
