d41886c13f0fd60a1172d34cb3bae95e23c6daf2
ColbyBouma <78135310+ColbyBouma@users.noreply.github.com>
2021-08-23 16:33:17 -0600

    Limit the number of results
    Fixes #74

--- PowerShell Scanners/Get Windows Update History/Get Windows Update History.ps1
+++ PowerShell Scanners/Get Windows Update History/Get Windows Update History.ps1
@@ -1,3 +1,8 @@
 & '.\Install and Import Module.ps1' -ModuleName "PSWindowsUpdate"
 
-Get-WUHistory
+# Retrieve the Row Limit from a log file.
+$RowLimit = [Int32](Get-Content .\Scanner.log | Select-String 'First = (\d+)').Matches.Groups[1].Value
+
+# -Last limits the number of results. This is necessary in Windows 10 2004 and later.
+# https://github.com/pdq/PowerShell-Scanners/issues/74
+Get-WUHistory -Last $RowLimit
