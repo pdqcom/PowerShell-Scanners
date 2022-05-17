91931ecd8572490c46c5018ef15c609a1ac316ce
Matt henry <matt.henry@owensboro.kyschools.us>
2022-05-17 15:59:53 -0500

    Removed old code. Added comments. Added some cosmetic spacing

--- PowerShell Scanners/Cipher Suite Detection/Cipher Suite Detection.ps1
+++ PowerShell Scanners/Cipher Suite Detection/Cipher Suite Detection.ps1
@@ -1,21 +1,4 @@
-#Query Registry for transport protocols
-
-#SSLv2
-$SSLv2Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server'
-$SSLv2Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' 
-
-#SSLv3
-$SSLv3Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' 
-$SSLv3Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' 
-
-#TLSv1.0
-$TLSv1Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' 
-$TLSv1Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' 
-
-#TLSv1.1
-$TLSv1_1Server = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' 
-$TLSv1_1Client = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' 
-
+# Create hashtable of protocols to check
 $Protocols = @{
     'SSL2.0 Server' = @{
         'Path'    = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server'
@@ -42,21 +25,34 @@ $Protocols = @{
         'Default' = $true
     }
 }
+
+# Create output object
 $output = [PSCustomObject]@{
     Host = $env:COMPUTERNAME
 }
+
+# Loop over protocol keys
 foreach ($Protocol in $Protocols.Keys) {
+    # Set status of the protocol to the configured default. Windows by default has them enabled (for now)
     $Status = $Protocols.$Protocol.Default
+
     if (Test-Path -Path $Protocols.$protocol.Path) {
+
+        # Get the property value of the registry path's Enabled key
         $EnablePropertyValue = (Get-ItemProperty -Path $Protocols.$protocol.Path | Select-Object -ExpandProperty Enabled -ErrorAction SilentlyContinue)
+
         if ($EnablePropertyValue -eq "0") {
             $Status = $false
         }
+
         if ($EnablePropertyValue -eq "1") {
             $Status = $true
         } 
     }
+
+    # Add this protocol name and the word Enabled along with the Status found to the output object
     Add-Member -InputObject $output -MemberType NoteProperty -Name ($Protocol + " Enabled") -value $Status
 }
 
+# Output the output object
 $output
