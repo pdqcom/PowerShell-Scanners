96620966c4c232e66970f426c08006c199296379
Petar Georgiev <pgeorgiev@codingburgas.bg>
2022-08-05 19:39:08 +0300

    Add PhysicalDbName property

--- PowerShell Scanners/SQL Server Databases/SQL Server Databases.ps1
+++ PowerShell Scanners/SQL Server Databases/SQL Server Databases.ps1
@@ -11,13 +11,13 @@ $query = @"
         ,suser_sname(owner_sid)
         ,create_date
         ,compatibility_level
-		,collation_name
-		,recovery_model_desc
-		
+        ,collation_name
+        ,recovery_model_desc
+        ,physical_database_name
     FROM 
         master.sys.databases
     WHERE
-        name NOT IN ('master','model','msdb','tempdb')
+        name NOT IN ('master', 'model', 'msdb', 'tempdb')
 
 "@
 
@@ -27,7 +27,7 @@ for($i = 2; $i -lt $dbs.Count - 2; $i++) {
     
     $tokens = $dbs[$i] -split "\|"
 
-    if ($tokens.Count -eq 8) {
+    if ($tokens.Count -eq 9) {
 
         [PSCustomObject]@{
             'Id' = [int]$tokens[0].Trim()
@@ -38,6 +38,7 @@ for($i = 2; $i -lt $dbs.Count - 2; $i++) {
             'CompatLevel' = [int]$tokens[5].Trim()
             'Collation' = $tokens[6].Trim()
             'RecoveryModel' = $tokens[7].Trim()
+            'PhysicalDbName' = $tokens[8].Trim()
         }
     }
 }
